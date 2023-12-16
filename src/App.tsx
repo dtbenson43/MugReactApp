4135ce9b0057cfeba8877b32057d01acd476dc7f
Daniel Benson <dbenson24@gmail.com>
2023-12-16 11:56:43 -0600

    add query hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useGetConversationsQuery } from './gql/types-and-hooks'
 
 function App() {
   const [count, setCount] = useState(0)
+  const result = useGetConversationsQuery();
+  console.log(result)
 
   return (
     <>
