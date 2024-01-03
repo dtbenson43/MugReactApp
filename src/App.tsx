9faaa026ec7eddbe6a16c64c1aee2c365d8fcdb2
Daniel Benson <dbenson24@gmail.com>
2024-01-02 22:36:30 -0600

    add header with login button

--- src/App.tsx
+++ src/App.tsx
@@ -1,93 +1,36 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-// import { useApolloClient } from '@apollo/client'
-import { useGetSimpleTestQuery } from "./gql/types-and-hooks";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
-import useApiClients from "./components/ApiProvider/useApiClients";
 import LoginCard from "./components/LoginCard/LoginCard";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
+  // DialogDescription,
+  // DialogHeader,
+  // DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const result = useGetSimpleTestQuery();
-  console.log(result);
-
-  // const client = useApolloClient();
-  const { mugClient, cosmosClient } = useApiClients();
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Input
-        type="email"
-        placeholder='Email'
-        onChange={(e) => setEmail(e.target.value)}
-        value={email}
-      />
-      <Input
-        type="password"
-        placeholder='Password'
-        onChange={(e) => setPassword(e.target.value)}
-        value={password}
-      />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Button onClick={async () => {
-          try {
-            const result = await mugClient.loginPost(true, false, {
-              email: email,
-              password: password
-            })
-            console.log(result)
-          } catch(err) {
-            console.log(err);
-          }
-        }} >Click Me</Button>
-        <Button onClick={async () => {
-          try {
-            const result = await cosmosClient.cosmosCosmostestGet()
-            console.log(result)
-          } catch(err) {
-            console.log(err);
-          }
-        }} >Next Click Me</Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      {/* <div className='flex items-center justify-center mx-auto h-screen'>
-        <LoginCard />
-      </div> */}
-      <Dialog>
-        <DialogTrigger>Open</DialogTrigger>
-        <DialogContent className="pt-0 pb-0">
-            <LoginCard withDialogClose className="w-full shadow-none border-none"/>
-        </DialogContent>
-      </Dialog>
+      <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container flex h-14 max-w-screen-2xl items-center">
+          <div></div>
+          <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button variant="outline">Login</Button>
+              </DialogTrigger>
+              <DialogContent className="pt-0 pb-0">
+                <LoginCard
+                  withDialogClose
+                  className="w-full shadow-none border-none"
+                />
+              </DialogContent>
+            </Dialog>
+          </div>
+        </div>
+      </header>
     </>
   );
 }
